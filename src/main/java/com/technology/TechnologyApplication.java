8b89e51b76087aa024dd3bd4dcbe5c826dd3dc38
huangzb <huangzb@bbxpc.com>
2018-11-13 18:08:54 +0800

    % 初始化工程目录结构

---
+++ src/main/java/com/technology/TechnologyApplication.java
@@ -0,0 +1,17 @@
+package com.technology;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: huangzhb
+ * @Date: 2018年11月13日 18:00:15
+ * @Description:
+ */
+@SpringBootApplication
+public class TechnologyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechnologyApplication.class);
+    }
+}
