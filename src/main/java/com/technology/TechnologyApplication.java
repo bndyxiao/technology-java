c3d114e768cf48e2d1d16beffac68450b12d3093
huangzb <huangzb@bbxpc.com>
2019-04-02 09:05:00 +0800

    * 增加是否手机浏览器校验

--- src/main/java/com/technology/TechnologyApplication.java
+++ src/main/java/com/technology/TechnologyApplication.java
@@ -1,6 +1,8 @@
 package com.technology;
 
 import com.github.pagehelper.PageHelper;
+import org.redisson.Redisson;
+import org.redisson.config.Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -39,5 +41,23 @@ public class TechnologyApplication extends SpringBootServletInitializer {
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
         return builder.sources(TechnologyApplication.class);
+    }
+
+
+
+    @Bean
+    Redisson redissonSentinel() {
+
+        // 支持单机，主从，哨兵，集群等模式
+        // 此为哨兵模式
+        Config config = new Config();
+//        config.useSentinelServers()
+//                .setMasterName("mymaster")
+//                .addSentinelAddress("redis://127.0.0.1:6379")
+//                .setPassword("123456");
+        // 使用单节点
+        config.useSingleServer().setAddress("127.0.0.1:6379");
+
+        return (Redisson) Redisson.create(config);
     }
 }
